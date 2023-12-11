aa9510c3f2ae98f6624538ef69ce530eb6d4bba8
Elton Chaves <82658356+eltonjfc@users.noreply.github.com>
2023-12-11 10:54:52 -0300

    Add files via upload

--- configure.py
+++ configure.py
@@ -1,16 +1,17 @@
 #!/bin/python3
 # Written by Elton Chaves
 
+from pbee import print_end
 from shutil import which
 import os, glob
 
 def configure_pbee():
-    PbeePATH = os.getcwd()
+    PbeePATH = os.path.dirname(__file__)
     
     # ---
     condition = os.path.isdir(PbeePATH)
     if condition is False:
-        print(f'error: invalid PbeePATH\n'); exit()
+        print(' error: invalid PbeePATH'); print_end
     else:
         return PbeePATH
 
@@ -32,7 +33,7 @@ def configure_basemodels(PbeePATH):
             if os.path.isfile(item) is True:
                 continue
             else:
-                print(f' requirement not found: {os.path.basename(item)}\n'); exit()
+                print(f' requirement not found: {item}'); print_end()
         return basemodels
     else:
         print(f' requirements not found\n'); exit()
@@ -41,11 +42,11 @@ def configure_sl(PbeePATH):
     superlearner = f'{PbeePATH}/super_learner_model.pkl'
     condition = os.path.isfile(superlearner)
     if condition is False:
-        print(f' requirement not found: super_learner_model.pkl\n'); exit()
+        print(f' requirement not found: super_learner_model.pkl\n'); print_end()
     else:
         return superlearner
 
-def requirements():
+def requirements(PbeePATH):
     RosettaPATHS = ['ROSETTA3', 'ROSETTA3_BIN', 'ROSETTA3_TOOLS']
     count = 0
     for item in RosettaPATHS:
@@ -54,11 +55,12 @@ def requirements():
             print(f' error (rosetta_path): {item} is not an environment variable.\n')
             count += 1; continue
     if count == 0:
-        b1 = glob.glob(f'{os.environ["ROSETTA3_BIN"]}/score_jd2.default.*')[0]
-        b2 = glob.glob(f'{os.environ["ROSETTA3_BIN"]}/rosetta_scripts.default.*')[0]
-        b3 = f'{os.environ["ROSETTA3_TOOLS"]}/protein_tools/scripts/clean_pdb.py'
-        b4 = f'{os.environ["ROSETTA3"]}/external/DAlpahBall/DAlphaBall.gcc'
-        requirements = [b1, b2, b3, b4]
+        r1 = glob.glob(f'{os.environ["ROSETTA3_BIN"]}/score_jd2.default.*')[0]
+        r2 = glob.glob(f'{os.environ["ROSETTA3_BIN"]}/rosetta_scripts.default.*')[0]
+        r3 = f'{os.environ["ROSETTA3_TOOLS"]}/protein_tools/scripts/clean_pdb.py'
+        r4 = f'{os.environ["ROSETTA3"]}/external/DAlpahBall/DAlphaBall.gcc'
+        r5 = f'{PbeePATH}/train_file.csv'
+        requirements = [r1, r2, r3, r4, r5]
         for item in requirements:
             condition = istool(item)
             if condition is False:
